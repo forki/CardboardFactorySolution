7c2748ceb7f308f6ca0b580ad9ce7f94bd5cdc42
Gleb Kruglov <kruglovgleb@gmail.com>
2017-07-16 23:56:51 +0300

    Domain.Core project

---
+++ Domain.Core/Cardboard.fs
@@ -0,0 +1,26 @@
+﻿namespace Domain.Core
+
+module Cardboard =
+    [<System.FlagsAttribute>]
+    type CorrugationTypes =
+        | E = 0b000001
+        | C = 0b000010
+        | EAndC = 0b000100
+        | AllWithoutPolygraphy = 0b000111
+        | Polygraphy = 0b010000
+        | All = 0b010111
+
+    type CardboardBrands =
+        | D = 1
+        | T = 2
+        | P = 3
+
+    type CardboardClasses =
+        | Class21 = 21
+        | Class22 = 22
+        | Class23 = 23
+
+    type CardboardColors =
+        | Brown = 1
+        | White = 2
+        | WhiteWhite = 3
