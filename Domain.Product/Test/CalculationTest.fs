7878502dcd56f0a2c1f7a58119dbc3e4a899d86e
Gleb Kruglov <kruglovgleb@gmail.com>
2017-07-19 00:08:20 +0300

    Moved ProductTypeTest to F# part 3

--- Domain.Product/Test/CalculationTest.fs
+++ Domain.Product/Test/CalculationTest.fs
@@ -32,7 +32,7 @@ module ProductTypeTest =
     }
 
     [<Test>]
-    let ``Calculate lengths for box0200``() =
+    let ``Calculate lengths for Box0200``() =
         let executionOne = CorrugationTypes.Enum.C ||| CorrugationTypes.Enum.EAndC;
         let executionTwo = CorrugationTypes.Enum.E;
 
@@ -92,7 +92,7 @@ module ProductTypeTest =
 
 
     [<Test>]
-    let ``Calculate lengths with errors``() =
+    let ``Calculate lengths with not existing argument``() =
         let lengthOneFormulas = new Dictionary<CorrugationTypes.Enum, Product.LengthFormula>()
         lengthOneFormulas.Add(CorrugationTypes.Enum.All, { 
             CorrugationType = CorrugationTypes.Enum.All; 
@@ -106,7 +106,19 @@ module ProductTypeTest =
             (fun () -> Calculation.calculateSheetSizes(CorrugationTypes.Enum.C, product) |> ignore)
             |> ignore
 
-        lengthOneFormulas.Clear()
+    [<Test>]
+    let ``Calculate lengths with no formula``() =
+        let lengthOneFormulas = new Dictionary<CorrugationTypes.Enum, Product.LengthFormula>()
+        let sub =  { defaultSubProduct with Product.SubProduct.LengthOneFormulas = lengthOneFormulas; }
+        let list = [sub]
+        let product = { defaultProductType with Name = "Test"; SubProducts = list }
+        Assert.Throws<Calculation.ProductTypeNoFormulaException>
+            (fun () -> Calculation.calculateSheetSizes(CorrugationTypes.Enum.C, product) |> ignore)
+            |> ignore
+
+    [<Test>]
+    let ``Calculate lengths with empty formula``() =
+        let lengthOneFormulas = new Dictionary<CorrugationTypes.Enum, Product.LengthFormula>()
         lengthOneFormulas.Add(CorrugationTypes.Enum.All, { 
             CorrugationType = CorrugationTypes.Enum.All; 
             FormulaText = String.Empty});
@@ -116,8 +128,10 @@ module ProductTypeTest =
         Assert.Throws<Calculation.ProductTypeNoFormulaException>
             (fun () -> Calculation.calculateSheetSizes(CorrugationTypes.Enum.C, product) |> ignore)
             |> ignore
-        
-        lengthOneFormulas.Clear()
+    
+    [<Test>]
+    let ``Calculate lengths with wrong formula``() =   
+        let lengthOneFormulas = new Dictionary<CorrugationTypes.Enum, Product.LengthFormula>()
         lengthOneFormulas.Add(CorrugationTypes.Enum.All, { 
             CorrugationType = CorrugationTypes.Enum.All; 
             FormulaText = "=-5224=+-342fff....+342---2342"});
@@ -127,3 +141,28 @@ module ProductTypeTest =
         Assert.Throws<Calculation.ProductTypeExpressionHasErrorException>
             (fun () -> Calculation.calculateSheetSizes(CorrugationTypes.Enum.C, product) |> ignore)
             |> ignore
+
+    [<Test>]
+    [<TestCase(CorrugationTypes.Enum.C, 120.0, 240.0, "[Length] + [Length]", "[Width] + [Length] + 50]", 240.0, 410.0)>]
+    [<TestCase(CorrugationTypes.Enum.E, 120.0, 120.0, "[Length] + [Width]", "[Width] + [Width] + 50]", 240.0, 290.0)>]
+    [<TestCase(CorrugationTypes.Enum.C, 400.0, 240.0, "[Length] * 2", "[Length] - [Width] + 50]", 800.0, 210.0)>]
+    let ``Calculate lengths with new values and formulas``(enum:CorrugationTypes.Enum, length:double, width:double, formulaLengthOne:string, formulaLengthTwo:string, result1:double, result2:double) =
+        let lengthOneFormulas = new Dictionary<CorrugationTypes.Enum, Product.LengthFormula>()
+        lengthOneFormulas.Add(CorrugationTypes.Enum.All, { 
+            CorrugationType = CorrugationTypes.Enum.All; 
+            FormulaText = formulaLengthOne});
+
+        let lengthTwoFormulas = new Dictionary<CorrugationTypes.Enum, Product.LengthFormula>()
+        lengthTwoFormulas.Add(CorrugationTypes.Enum.All, { 
+            CorrugationType = CorrugationTypes.Enum.All; 
+            FormulaText = formulaLengthTwo});
+        let sub =  { Product.SubProduct.Name = "Sub"; Product.SubProduct.LengthOneFormulas = lengthOneFormulas; Product.SubProduct.LengthTwoFormulas = lengthTwoFormulas }
+        let list = [sub]
+        let product = { defaultProductType with Name = "Test"; SubProducts = list }
+        product.Parameters.[lengthName] <- { product.Parameters.[lengthName] with Value = Nullable<double>(length)}
+        product.Parameters.[widthName] <- { product.Parameters.[lengthName] with Value = Nullable<double>(width)}
+
+        let sheetSizes = Calculation.calculateSheetSizes(enum, product)
+        Assert.AreEqual(1, sheetSizes.Length)
+        Assert.AreEqual(result1, sheetSizes.[0].LengthOne);
+        Assert.AreEqual(result2, sheetSizes.[0].LengthTwo);
